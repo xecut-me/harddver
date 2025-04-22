78470c50c6e79dc896318016778e521be49d96d4
Evgenii Novikov <enovikov11@yandex.com>
2025-04-22 21:35:01 +0200

    fix

--- static/main.js
+++ static/main.js
@@ -43,7 +43,7 @@ async function getBackdoorState() {
 }
 
 function addMessage(message) {
-    querySelector("#chatWidget").innerText = `@${message.username}: ${message.text}\n` + querySelector("#chatWidget").innerText;
+    document.querySelector("#chatWidget").innerText = `@${message.username}: ${message.text}\n` + querySelector("#chatWidget").innerText;
 }
 
 function onMessage(updateJson) {
